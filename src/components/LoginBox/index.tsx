7e7e6a7a9989fc4019de180ac3eb6acf98839979
Thierry Santos <ThierryPitela@hotmail.com>
2022-01-18 11:57:28 -0300

    Atualização dos componentes

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,16 +1,17 @@
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth';
 
-import { useAuth } from '../../hooks/useAuth';
 import styles from './styles.module.scss';
 
 export function LoginBox() {
-  const { signInUrl } = useAuth()
+  const { signInUrl } = useContext(AuthContext);
 
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href={signInUrl} className={styles.signWithGithubButton}>
-        <VscGithubInverted size={24} />
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
         Entrar com Github
       </a>
     </div>
